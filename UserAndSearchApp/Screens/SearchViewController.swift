f05d7661f1091e3946d8133a23474f6b6411442d
yasemintok <tokyasemin27@gmail.com>
2022-06-26 17:48:33 +0300

    firstCommit

---
+++ UserAndSearchApp/Screens/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserAndSearchApp
+//
+//  Created by Yasemin TOK on 26.06.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
